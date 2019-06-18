23a97e5a21823b9a0d33b9a5842f08de812a2d18
Khang <ankhangtran1996@gmail.com>
2019-06-19 00:41:06 +0700

    Update
     use npm install --save react-native-gesture-handler to fix bug

---
+++ SmartHome/screens/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Dashboard </Text>
+      </View>
+    );
+  }
+}
+export default Dashboard;
